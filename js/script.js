a1a5f40651272a709f0df166368316c4d0995632
Roger Hagerbaum <roger.hagerbaum@lerniastudent.se>
2021-09-30 12:09:34 +0200

    add checked functionality and count

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+let completedCount = 0;
 // array and index
 const toDoArray = [];
 let index = 0;
@@ -6,7 +6,7 @@ let index = 0;
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 const toDoList = document.querySelector("#toDoList");
-
+const completedTasks = document.querySelector("p");
 
 button.addEventListener("click", function() {
 // Get the value and create list and span tag
@@ -14,10 +14,27 @@ const toDoTask = input.value;
 const task = document.createElement("li");
 const taskText = document.createElement("span");
 taskText.innerText = toDoTask;
+// Check uncheced function and completed counter
+taskText.addEventListener("click", function(){
+    if(toDoObj.checked == false){
+    task.classList.toggle("checked");
+    toDoObj.checked = true;
+    completedCount++;
+    }
+    else{
+        task.classList.toggle("checked");
+        toDoObj.checked = false;
+        completedCount--;   
+    }
+    
+    completedTasks.innerText = `${completedCount} completed`;
+
+});
 // create a object with the li and span tag
 let toDoObj = {
     "li": task,
-    "span": taskText
+    "span": taskText,
+    "checked": false
 };
 //push the objekt into the array
 toDoArray.push(toDoObj);
@@ -28,6 +45,8 @@ toDoList.appendChild(toDoArray[index].li);
 task.appendChild(toDoArray[index].span);
 index ++;
 
+console.log(completedCount);
+input.value = "";
 })
 
 
